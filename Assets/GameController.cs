29bed24a2e97629eebf5fdad0cd221198b918bb9
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-04-01 12:27:17 -0500

    4/1/2023

---
+++ Assets/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+
+public class GameController : MonoBehaviour
+{{
+
+   
+    using UnityEngine.UI;
+    using UnityEngine.SceneManagement;
+   
+   [Header("Cube Object")]
+   public GameObject currentCube;
+   [Header("Last Cube Object")]
+   public Text text;
+   [Header("Current Level")]
+   public int Level;
+   [Header("Boolean")]
+   public bool Done;
+
+}
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
