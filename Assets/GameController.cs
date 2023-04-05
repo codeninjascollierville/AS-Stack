8fac0ce1c496e5d1b7dceb0d40addfac2d2dcbba
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-04-05 17:39:38 -0400

    4/5/2023

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
  
 
 public class GameController : MonoBehaviour
-{{
+{
 
-   
-    using UnityEngine.UI;
-    using UnityEngine.SceneManagement;
-   
+
+   //{
+    
+//}
    [Header("Cube Object")]
    public GameObject currentCube;
    [Header("Last Cube Object")]
@@ -20,7 +21,7 @@ public class GameController : MonoBehaviour
    [Header("Boolean")]
    public bool Done;
 
-}
+
 
    
     // Start is called before the first frame update
@@ -34,4 +35,20 @@ public class GameController : MonoBehaviour
     {
         
     }
+
+void Newblock()
+{
+    if(lastCube != null)
+    {
+        currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
+        currentCube.transform.position.y,
+        Mathf.Round(currentCube.transform.position.z));
+        currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                 lastCube.transform.localScale.y,
+                                 lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+                                 currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5) + Vector3.up * 5f;
+        
+    }
+}
+
 }
